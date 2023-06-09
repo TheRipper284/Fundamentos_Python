fee30b4f601731863a15b3e205e4b3a6311bab0d
TherRipper482 <santiagogustavo882@gmail.com>
2023-06-09 16:52:15 -0600

    04 Operadores

---
+++ operadores.py
@@ -0,0 +1,19 @@
+# operadores matematicos
+# + - * %
+# / Divición float
+# // Divición entera
+# ** Potencia
+print(12 + 12)
+print(12 - 12)
+print(12 * 12)
+print(12 % 12)
+print(10 / 3)
+print(10 // 2)
+print(2 ** 16)
+
+#operadores para string
+# + para concatenar
+# - para replicar
+print("-------" * 4)
+print("hola " + "Mundo!")
+print("Hola " * 3)
