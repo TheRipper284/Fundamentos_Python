928e0c0af589f47ff04f23733d574ea6325cca46
TherRipper482 <santiagogustavo882@gmail.com>
2023-06-13 12:45:42 -0600

    06 Condicionales

---
+++ condicionales.py
@@ -0,0 +1,22 @@
+# if, elif, else
+# if expresion_booleana:
+#   bloque de código
+
+age = 18
+
+if age >= 18:
+    print("Es Mayor de Edad")
+
+if age < 18:
+    print("Es Menor De Edad")
+else:
+    print("Es Mayor de Edad")
+
+age =75
+
+if age > 18:
+    print("Es Menor De Edad")
+elif age > 70:
+    print("Es Mayor de Edad")
+else:
+    print ("Es Adulto Mayor")    
--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 # Fuction print
 print("Hola Mundo!")
 print("Soy Ese")
+
+print("Podemos Evitar Diferentes Tipos de Dtaos")
+print(12)
+print(12.23)
+print(True)
+print([])
